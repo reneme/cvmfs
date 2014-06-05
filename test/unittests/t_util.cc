7e91336eb3a6f5789ac7c81f7e049a5536d20d78
Rene Meusel <rene@renemeusel.de>
2014-06-05 13:18:46 +0000

    TEST: basic unit test for LazyInitializer<>

--- test/unittests/t_util.cc
+++ test/unittests/t_util.cc
@@ -47,3 +47,30 @@ TEST(T_Util, IsAbsolutePath) {
   const bool absolute = IsAbsolutePath("/tmp/foo.bar");
   EXPECT_TRUE (absolute) << "absolute path not recognized";
 }
+
+
+//
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+//
+
+
+int lazy_initializer_called = 0;
+const int lazy_initializer_value = 1337;
+void lazy_initializer(int * const &i) {
+  *i = lazy_initializer_value;
+  ++lazy_initializer_called;
+}
+
+TEST(T_Util, LazyInitializer) {
+  ASSERT_EQ (0, lazy_initializer_called);
+  LazyInitializer<int> i(LazyInitializer<int>::MakeCallback(&lazy_initializer));
+
+  EXPECT_EQ (0, lazy_initializer_called);
+  int i_res_1 = i.Get();
+  EXPECT_EQ (1, lazy_initializer_called);
+  EXPECT_EQ (lazy_initializer_value, i_res_1);
+
+  int i_res_2 = i.Get();
+  EXPECT_EQ (1, lazy_initializer_called);
+  EXPECT_EQ (lazy_initializer_value, i_res_2);
+}
